f85b7ad66a27eee37d80463344c15691d2de32cc
Marko <129887087+Marko010101@users.noreply.github.com>
2025-03-03 20:43:38 +0400

    Initial commit & implemented most parts of the About, Account, and Cabins pages..

---
+++ app/_components/CabinList.js
@@ -0,0 +1,25 @@
+import { unstable_noStore } from "next/cache.js";
+import { getCabins } from "../_lib/data-service.js";
+import CabinCard from "@/app/_components/CabinCard.js";
+
+export default async function CabinList({ filter }) {
+  // unstable_noStore();
+  const cabins = await getCabins();
+
+  let displayedCabins;
+
+  if (filter == "all") displayedCabins = cabins;
+  if (filter == "small") displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  if (filter == "medium") displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7);
+  if (filter == "large") displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+
+  if (!cabins.length) return null;
+
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {displayedCabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+}
---
+++ app/_components/Spinner.js
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
